553c0d337072efa9aacb55f0555c292c32d4154c
khushi tomar <khushitomar2001@gmail.com>
2022-06-12 20:12:59 +0530

    rounded button

--- app/src/main/java/com/example/hehe/MainActivity.java
+++ app/src/main/java/com/example/hehe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hehe;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,44 +8,39 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    ArrayList barArraylist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barchart);
+        BarChart barChart = findViewById(R.id.barchart);
+        getData();
+        BarDataSet barDataSet = new BarDataSet(barArraylist,"example");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(true);
 
-        BarChart barChart = (BarChart) findViewById(R.id.barchart);
+    }
 
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(8f, 0));
-        entries.add(new BarEntry(2f, 1));
-        entries.add(new BarEntry(5f, 2));
-        entries.add(new BarEntry(20f, 3));
-        entries.add(new BarEntry(15f, 4));
-        entries.add(new BarEntry(19f, 5));
-
-        BarDataSet bardataset = new BarDataSet(entries, "Cells");
-
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("2016");
-        labels.add("2015");
-        labels.add("2014");
-        labels.add("2013");
-        labels.add("2012");
-        labels.add("2011");
-
-        BarData data = new BarData(labels, bardataset);
-        barChart.setData(data); // set the data and list of labels into chart
-        barChart.setDescription("Set Bar Chart Description Here");  // set the description
-        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
-        barChart.animateY(5000);
+    private void  getData()
+    {
+     barArraylist = new ArrayList();
+     barArraylist.add(new BarEntry(2f,10));
+     barArraylist.add(new BarEntry(3f,20));
+     barArraylist.add(new BarEntry(4f,30));
+     barArraylist.add(new BarEntry(5f,40));
 
     }
 }
-
-
