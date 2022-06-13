06d7b152e0b9bed853fa669caa9cf2f860ba54e4
khushi tomar <khushitomar2001@gmail.com>
2022-06-13 19:34:13 +0530

    barchart

--- app/src/main/java/com/example/hehe/MainActivity.java
+++ app/src/main/java/com/example/hehe/MainActivity.java
@@ -1,46 +1,26 @@
 package com.example.hehe;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity
-{
-    ArrayList barArraylist;
-
+public class MainActivity extends AppCompatActivity {
+    private Button nextbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.barchart);
-        BarChart barChart = findViewById(R.id.barchart);
-        getData();
-        BarDataSet barDataSet = new BarDataSet(barArraylist,"example");
-        BarData barData = new BarData(barDataSet);
-        barChart.setData(barData);
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        barDataSet.setValueTextColor(Color.BLACK);
-        barDataSet.setValueTextSize(16f);
-        barChart.getDescription().setEnabled(true);
-
-    }
-
-    private void  getData()
-    {
-     barArraylist = new ArrayList();
-     barArraylist.add(new BarEntry(2f,10));
-     barArraylist.add(new BarEntry(3f,20));
-     barArraylist.add(new BarEntry(4f,30));
-     barArraylist.add(new BarEntry(5f,40));
-
+        setContentView(R.layout.activity_main);
+        nextbtn = findViewById(R.id.idBtnRegister);
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this, Bar_Chart.class);
+                startActivity(i);
+            }
+        });
     }
 }
